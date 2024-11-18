7b0b23ff192d7818ab75ccda575a2a1c4d95d348
Lauri <lauri.etelamaki@gmail.com>
2024-11-18 17:21:56 +0200

    Added small demo

--- Readme.md
+++ Readme.md
@@ -2,7 +2,7 @@
 An simple 3D using OpenGL 1.1 and OS-native window management libraries.
 Dependencies: X11, GLU, OpenGL
 
-![alt text](dev/screenshots/teapot.png "Title")
+![alt text](dev/screenshots/demo18_11_24.png "Title")
 
 ## Collaboration
 Please read the syntax.md document for information on code syntax and collaboration rules.
--- src/main.c
+++ src/main.c
@@ -14,7 +14,8 @@ int main(int argc, char** argv)
     picg_gl_setClearColor(0.5, 0.5, 1.0, 1.0);
 
     picg_mesh* teapot = picg_modelObj_create("dev/Models/teapot.obj");
-    picg_mesh* rects = picg_modelObj_create("dev/Models/cube.obj");
+    picg_mesh* cube = picg_modelObj_create("dev/Models/cube.obj");
+    picg_mesh* cow = picg_modelObj_create("dev/Models/cow.obj");
     
 
     GLfloat pos[4];
@@ -35,15 +36,22 @@ int main(int argc, char** argv)
     for(;;) {
         picg_gl_clear();
         picg_mesh_render(teapot);
-        picg_mesh_render(rects);
+        picg_mesh_render(cube);
+        picg_mesh_render(cow);
 
 
-        teapot->rotation.y += 0.5;
-        teapot->position.z += 0.01;
-        rects->position.z -= 0.1;
-        rects->rotation.z -= 0.5;
-        rects->rotation.y -= 0.5;
-        rects->rotation.x -= 0.5;
+        teapot->rotation.y += 1.5;
+        teapot->position.z -= 0.1;
+
+        cube->rotation.z -= 0.5;
+        cube->rotation.y -= 0.5;
+        cube->rotation.x -= 0.5;
+        cube->position.x -= 0.1;
+        cube->position.z -= 0.15;
+
+        cow->position.z -= 0.05;
+        cow->rotation.y -= 1.0;
+        cow->position.x += 0.05;
 
         picg_window_swapbuffers();
         picg_gl_flush();
