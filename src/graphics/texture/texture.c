85920c0b3ea0661fb89970294d632991d6314341
Lauri <lauri.etelamaki@gmail.com>
2025-06-05 10:19:55 +0300

    memory management

--- src/graphics/texture/texture.c
+++ src/graphics/texture/texture.c
@@ -1,4 +1,5 @@
 #include "texture.h"
+#include <stb_image.h>
 
 void picg_texture_load(picg_image* image)
 {
@@ -13,8 +14,8 @@ void picg_texture_load(picg_image* image)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width, image->height, 0, GL_RGB, GL_UNSIGNED_BYTE, image->data);
-    //stbi_image_free(image.data);
-
+    
+    stbi_image_free(image->data);
     glBindTexture(GL_TEXTURE_2D, 0);
     glDisable(GL_TEXTURE_2D);
 }
